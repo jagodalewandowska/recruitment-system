42ea835a0af1acff813ea3c8f399657eb1d0422b
blaszu000 <blazejszulc98@gmail.com>
2023-12-06 21:21:51 +0100

    UserControllerTest

---
+++ backend/src/test/java/pbs/edu/rekrutacja/UserControllerTest.java
@@ -0,0 +1,58 @@
+package pbs.edu.rekrutacja;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pbs.edu.rekrutacja.controllers.UserController;
+import pbs.edu.rekrutacja.models.User;
+import pbs.edu.rekrutacja.services.UserService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    void getAllUsers_shouldReturnListOfUsers() throws Exception {
+        List<User> users = Arrays.asList(new User("John", "john@example.com", "password"),
+                new User("Jane", "jane@example.com", "password"));
+
+        when(userService.getAllUsers()).thenReturn(users);
+
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].username").value("John"))
+                .andExpect(jsonPath("$[1].username").value("Jane"));
+
+        verify(userService, times(1)).getAllUsers();
+        verifyNoMoreInteractions(userService);
+    }
+}

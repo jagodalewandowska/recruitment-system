5e06479bf8101cc7c1c5e4b084bf6c56309f9191
blaszu000 <blazejszulc98@gmail.com>
2023-12-06 21:43:12 +0100

    TestControllerTest

---
+++ backend/src/test/java/pbs/edu/rekrutacja/TestControllerTest.java
@@ -0,0 +1,28 @@
+package pbs.edu.rekrutacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testAllAccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/test/all")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Public Content."));
+    }
+
+}

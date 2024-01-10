baf1c0da8f2fded4054b49f6f32d2ac576d3a66e
jagodalewandowska <jadlewandowska@gmail.com>
2024-01-10 20:34:39 +0100

    Some progress when fixing updating password when updating a user (todo). Roles are being added

--- backend/src/main/java/pbs/edu/rekrutacja/controllers/UserController.java
+++ backend/src/main/java/pbs/edu/rekrutacja/controllers/UserController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.*;
 import pbs.edu.rekrutacja.models.ERole;
 import pbs.edu.rekrutacja.models.Role;
 import pbs.edu.rekrutacja.models.User;
+import pbs.edu.rekrutacja.repository.RoleRepository;
 import pbs.edu.rekrutacja.services.UserService;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+
+import static pbs.edu.rekrutacja.models.ERole.ROLE_USER;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -18,6 +22,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    private final RoleRepository roleRepository;
+
+    public UserController(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
     @GetMapping
     public List<User> getAllUsers() {
@@ -31,13 +41,32 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(userRole);
+        user.setRoles(roles);
         return userService.createUser(user);
     }
 
     @PutMapping("/{userId}")
     public User updateUser(@PathVariable Long userId, @RequestBody User newUser) {
-        System.out.println("Received PUT request for user update. User ID: " + userId);
-        return userService.updateUser(userId, newUser);
+        User existingUser = userService.getUserById(userId);
+        existingUser.setUsername(newUser.getUsername());
+        existingUser.setEmail(newUser.getEmail());
+        existingUser.setFirstName(newUser.getFirstName());
+        existingUser.setLastName(newUser.getLastName());
+        existingUser.setAddress(newUser.getAddress());
+        existingUser.setCity(newUser.getCity());
+        existingUser.setPostalCode(newUser.getPostalCode());
+        existingUser.setExperience(newUser.getExperience());
+        existingUser.setPhoneNumber(newUser.getPhoneNumber());
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(userRole);
+        existingUser.setRoles(roles);
+        return userService.updateUser(userId, existingUser);
     }
 
 

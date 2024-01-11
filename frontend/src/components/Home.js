1ff0bd332f43cbbc920c08b3c5713707affbe464
jagodalewandowska <jadlewandowska@gmail.com>
2024-01-11 20:02:35 +0100

    Sorting job offers by name

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,17 +3,31 @@ import axios from "axios";
 
 const Home = () => {
     const [jobs, setJobs] = useState([]);
+    const [sortOrder, setSortOrder] = useState("asc"); // "asc" or "desc"
 
     useEffect(() => {
         axios
             .get("http://localhost:8082/api/jobs")
             .then((response) => {
-                setJobs(response.data);
+                const sortedJobs = sortJobs(response.data, sortOrder);
+                setJobs(sortedJobs);
             })
             .catch((error) => {
                 console.error("Error fetching jobs:", error);
             });
-    }, []);
+    }, [sortOrder]);
+
+    const toggleSortOrder = () => {
+        const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+        setSortOrder(newSortOrder);
+    };
+
+    const sortJobs = (jobsToSort, order) => {
+        return jobsToSort.sort((a, b) => {
+            const comparison = a.title.localeCompare(b.title);
+            return order === "asc" ? comparison : -comparison;
+        });
+    };
 
     return (
         <div className="container mt-4">
@@ -21,7 +35,10 @@ const Home = () => {
             <table className="table">
                 <thead>
                 <tr>
-                    <th scope="col">Nazwa</th>
+                    <th scope="col" onClick={toggleSortOrder}>
+                        {sortOrder === "asc" ? " ▲" : " ▼"}
+                         Nazwa
+                    </th>
                     <th scope="col">Opis</th>
                 </tr>
                 </thead>
